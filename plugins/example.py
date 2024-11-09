58e12ff4ce7db5494dfb1641a54b2b63dc653032
Mnzn <119044297+MnznSama@users.noreply.github.com>
2024-11-09 19:38:13 +0800

    Add files via upload

--- plugins/example.py
+++ plugins/example.py
@@ -3,8 +3,6 @@ from utils.operate import send_group_message, send_private_message
 from utils.events import GroupMessage, PrivateMessage
 from utils import log
 
-
-
 info = {
     "name": "Example",
     "description": "A Example Plugin",
@@ -18,7 +16,6 @@ def group_message(message):
     message_event = GroupMessage(json.loads(message))
     if message_event.raw_message == "test":
         logger.debug("Module:Test-Success")
-        print(1)
         send_group_message(message_event.group_id, "Module:Test-Success")
         return 1
     else:
