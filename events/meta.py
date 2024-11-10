4c1a19a9a4c3c9497e4d00e94bc068252e4f0092
Mnzn <119044297+MnznSama@users.noreply.github.com>
2024-11-10 18:41:25 +0800

    细化

--- events/meta.py
+++ events/meta.py
@@ -3,7 +3,7 @@ from utils import log
 def main(message):
     logger = log.get_logger()
     if message.get("meta_event_type") == "heartbeat":
-        logger.debug("Get-HeartBeat-" + str(message.get("status").get("online")) + "-" + str(
+        logger.debug("心跳-" + str(message.get("status").get("online")) + "-" + str(
             message.get("status").get("good")) + "-" + str(message.get("time")))
     if message.get("meta_event_type") == "lifecycle":
-        logger.debug("LifeCycle-" + message.get("sub_type") + "-" + str(message.get("self_id")))
+        logger.debug("生命周期-" + message.get("sub_type") + "-" + str(message.get("self_id")))
